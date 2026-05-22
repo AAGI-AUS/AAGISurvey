e53f440fb3376a73ba91627e5bb6b069343d5b64
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-05-22 15:40:34 +0800

    chore: shorten long ling

--- R/utilities.R
+++ R/utilities.R
@@ -1,4 +1,4 @@
-#' Return the left-hand side if it's not NULL, otherwise it return the right-hand side
+#' Return the left-hand side if it's not NULL, else return the right-hand side
 #' @dev
 `%||%` <- function(x, y) {
   if (is.null(x)) y else x
