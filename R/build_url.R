1b9212a13020dc213e83000152dacab542fa73ca
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-05-25 12:45:55 +0800

    feat: export build_url() for use in AAGISurveyApp

---
+++ R/build_url.R
@@ -0,0 +1,46 @@
+#' Create the URL string from user provided values
+#'
+#' Create the URL string from user provided values for returning to the user.
+#'
+#' @param base Character string of base URL.
+#' @param support_type Character vector of support types.
+#' @param design_type Character string of design type.
+#' @param analysis_type Character string of analysis type.
+#' @param aagi_node Character string of AAGI node.
+#' @param organisation_type Character string of organisation type.
+#'
+#' @examples
+#' build_url(
+#'   "https://curtin.au1.qualtrics.com/jfe/form/SV_eXLvfgMz58RktQa",
+#'   "S_D",
+#'   "D_SP",
+#'   "CU",
+#'   "O_ACA"
+#' )
+#'
+#' @export
+#' @returns The full URL string.
+
+build_url <- function(
+  base,
+  support_type,
+  design_type,
+  analysis_type,
+  aagi_node,
+  organisation_type
+) {
+  params <- list(
+    ST = paste(support_type, collapse = ","),
+    DT = design_type %||% "",
+    AT = analysis_type %||% "",
+    AN = aagi_node %||% "",
+    OT = organisation_type %||% ""
+  )
+  query <- paste(
+    names(params),
+    vapply(params, utils::URLencode, character(1L), reserved = TRUE),
+    sep = "=",
+    collapse = "&"
+  )
+  sprintf("%s?%s", base, query)
+}
